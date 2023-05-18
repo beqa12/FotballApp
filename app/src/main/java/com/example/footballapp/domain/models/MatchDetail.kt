63c6b12bdcda3c77f75fa6b4da5ea6c089745c20
beqa12 <bchxikvadze12@gmail.com>
2023-05-18 17:03:09 +0400

    Adatper Viewhoders layouts

--- app/src/main/java/com/example/footballapp/domain/models/MatchDetail.kt
+++ app/src/main/java/com/example/footballapp/domain/models/MatchDetail.kt
@@ -1,5 +1,54 @@
 package com.example.footballapp.domain.models
 
+import com.example.footballapp.data.models.*
+import java.io.Serializable
+
 data class MatchDetail(
-    var id: String
+    var resultCode: Int?,
+    var match: Match
 )
+
+data class Match(
+    var team1: TeamInformation?,
+    var team2: TeamInformation?,
+    var matchTime: Double?,
+    var matchDate: Long?,
+    var stadiumAdress: String?,
+    var matchSummary: MatchSummary
+): Serializable
+
+
+data class TeamInformation(
+    var teamName: String?,
+    var teamImage: String?,
+    var score: Int?,
+    var ballPosition: Int?
+): Serializable
+
+data class MatchSummary(
+    var matchSummaries: List<MatchSummaries>?
+): Serializable
+
+data class MatchSummaries(
+    var actionTime: String?,
+    var team1Action: List<TeamAction>?,
+    var team2Action: List<TeamAction>?
+): Serializable
+
+data class TeamAction(
+    var actionType: MatchActionType?,
+    var teamType: MatchTeamType?,
+    var action: Action
+): Serializable
+
+data class Action(
+    var player: Player?,
+    var player1: Player?,
+    var player2: Player?,
+    var goalType: GoalType?
+): Serializable
+
+data class Player(
+    var playerName: String?,
+    var playerImage: String?
+): Serializable
--- app/src/main/java/com/example/footballapp/domain/repository/MatchDetailsRepo.kt
+++ app/src/main/java/com/example/footballapp/domain/repository/MatchDetailsRepo.kt
@@ -1,4 +1,5 @@
 package com.example.footballapp.domain.repository
 
 interface MatchDetailsRepo {
+
 }
